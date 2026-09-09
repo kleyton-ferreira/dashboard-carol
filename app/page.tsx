b8aafd5cee704e0e81a93b5f3b534887d2f9bea9
Kleyton Ferreira <kleytondev40@gmail.com>
2026-09-09 19:16:57 -0300

    refactor: home page components

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,5 @@
-import Layout from "./_components/layout";
-
 const HomePage = async () => {
-  return (
-    <>
-      <div>
-        <h1>Seja bem vindo</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat,
-          blanditiis consectetur. Commodi ullam, quod aliquam adipisci tempore
-          officia, fugit voluptatum non aspernatur quaerat obcaecati, odit
-          possimus debitis perferendis expedita illum?
-        </p>
-      </div>
-    </>
-  );
+  return <div></div>;
 };
 
 export default HomePage;
