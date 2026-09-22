eecd39dea531909983f15b86e6ba8cfa147ae234
Kleyton Ferreira <kleytondev40@gmail.com>
2026-09-22 16:12:29 -0300

    refactor: ensure revenue is fetched by sale date

--- app/_data-access/dashboard/get-dashboard.ts
+++ app/_data-access/dashboard/get-dashboard.ts
@@ -28,7 +28,8 @@ export const getDashboard = async (): Promise<DashboardDto> => {
             `
         SELECT COALESCE(SUM("unitPrice" * "quantity"), 0) as "totalRevenue"
         FROM "SaleProduct"
-        WHERE "createdAt" > $1 AND "createdAt" < $2
+        JOIN "Sale" ON "SaleProduct"."saleId" = "Sale"."id"
+        WHERE "Sale"."date" > $1 AND "Sale"."date" < $2
         `,
             day.startOf("day").toDate(),
             day.endOf("day").toDate()
@@ -44,12 +45,14 @@ export const getDashboard = async (): Promise<DashboardDto> => {
     const totalRevenueQuery = `
      SELECT COALESCE(SUM("unitPrice" * "quantity"), 0) as "totalRevenue"
      FROM "SaleProduct"
+     JOIN "Sale" ON "SaleProduct"."saleId" = "Sale"."id"
     `
 
     const todayRevenueQuery = `
      SELECT COALESCE(SUM("unitPrice" * "quantity"), 0) as "todayRevenue"
      FROM "SaleProduct"
-     WHERE "createdAt" > $1 AND "createdAt" < $2
+     JOIN "Sale" ON "SaleProduct"."saleId" = "Sale"."id"
+     WHERE "Sale"."date" > $1 AND "Sale"."date" < $2
     `
 
     const startOfDay = new Date(new Date().setHours(0, 0, 0, 0));
